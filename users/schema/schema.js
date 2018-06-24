b3e24e2e8ba18c73592d3bdefa2eccdb21fec2ff
Sean M <sean@arrowburn.co.nz>
2018-06-25 11:00:46 +1200

    nested queries

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -6,13 +6,23 @@ const {
     GraphQLInt, 
     GraphQLSchema 
 } = graphql;
-
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
 const UserType = new GraphQLObjectType({
     name: "User",
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt}
+        age: { type: GraphQLInt},
+        company: {
+            type: CompanyType
+        }
     }
 });
 
