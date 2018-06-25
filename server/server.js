ecd278735955b5d4e9c5189e0890b1162ffaa290
Sean M <sean@arrowburn.co.nz>
2018-06-26 10:15:50 +1200

    setup Apollo Client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://sean:michigan7@ds219181.mlab.com:19181/lyrical-graphql-react';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
